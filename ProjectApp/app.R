86b9f44efa29c6d33b86ab5b5bf180a754a4388c
davinchi73 <dev107c.d133@gmail.com>
2024-04-22 14:57:48 -0500

    some app work

--- ProjectApp/app.R
+++ ProjectApp/app.R
@@ -1,6 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application created by: Chris and Devinn (AKA, the RStudio Bad Boys)
 #
 # Find out more about building applications with Shiny here:
 #
@@ -27,42 +26,61 @@ steamDataGenres <- read_csv("../../data/data_clean/steamDataGenres.csv")
 steamDataDevelopers <- read_csv("../../data/data_clean/steamDataDevelopers.csv")
 
 
-# Define UI for application that draws a histogram
+# ui design start here
 ui <- fluidPage(
 
     # Application title
-    titlePanel("General Steam data"),
+    titlePanel("Steam Data Exploration"),
     
     # Tab layout
     tabsetPanel(
       
+      # TAB1 -------------------------------------------------------------------------------------------------------------------------------------
       tabPanel("Info Page",
                
                fluidRow(
                  column(12, 
-                        verbatimTextOutput("info_paragraph")
+                        uiOutput("info_paragraph")
                  )
                )
         
       ),
       
+      # TAB2 -----------------------------------------------------------------------------------------------------------------------------------
+      
+      tabPanel("User Guide",
+               
+               fluidRow(
+                 column(12, 
+                        uiOutput("directions_page")
+                 )
+               )
+      ),
+      
+      # TAB3 -----------------------------------------------------------------------------------------------------------------------------------
+      
       tabPanel("Explore Steam Data",
                
+               # Plot select (switch between plots)
                selectInput("plot_select", "Select Plot", 
                            choices = c("Density Plot of Prices", 
                                        "Developer Size vs Price", 
                                        "Average Price Over Years")),
                
+               # plot
                plotOutput("explore_plots")
                ),
       
+      # TAB4 -----------------------------------------------------------------------------------------------------------------------------------
       tabPanel("Explore Price to User rating",
                
+               # Rating select (switch between specific rating scale)
                selectInput("rating_select", "Select rating", 
                            choices = c("Steam Userscore", 
                                        "IGDB Userscore", 
                                        "Metacritic Userscore")),
                
+               # Genre select (switch between most popular game genres)
                selectInput("genre_select", "Select game genre", 
                            choices = c("Adventure", 
                                        "Casual", 
@@ -72,15 +90,18 @@ ui <- fluidPage(
                                        "RPG",
                                        "Simulation")),
                
+               # Price select (change the max price value on x axis to see large scale vs small scale shifts)
                sliderInput("price_select",
                            "Price parameter",
                            min = 0,
                            max = 70,
                            value = 35),
                
+               # plot
                plotOutput("price_to_rating")
       )
       
+      # END TABS --------------------------------------------------------------------------------------------------------------------------------
     )
     
 )
@@ -88,47 +109,90 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$info_paragraph <- renderText({
-    "- Our project is focused on one of the largest sources of creative output of the 21st century: video games. 
-     Video games frequently employ detailed narratives, sophisticated and challenging gameplay, and can connect 
-     with profound elements of the human psyche. We’ve decided to look at trends in video games as a way to
-     assess what draws people to particular games.
-
-    - In large part, we’ve kept our exploration open-ended, but we’ve focused on the factors that influence the 
-    ratings and popularity of video games on Steam, one of the largest platforms for purchasing and playing video 
-    games, with an estimated 60% of total PC game revenue captured by the platform.* 
-    
-    - We first were interested in any possible relationship between price and developer size. At first 
-    glance, however, a clear relationship is not apparent. However, it is interesting to note that there are relatively 
-    few large developers (measured by the number of Steam games published), which may warrant further investigation. 
-    It might be worthwhile to look at large developers and small developers separately.
-    
-    - Diving in deeper to the data, we decided to investigate price vs user ratings across three different websites. 
-    We chose to look closely at the ratings from users on Steam (of course), IGDB, and Metacritic. Each of these 
-    websites are highly active and have a large number of users. Our rationale was that different categories of games 
-    may inherently have higher development costs, or may  be subject to different kinds of pressures from their users. 
-    Overall, we found a weak, positive relationship between price and popularity. One possibility for this relationship 
-    existing at all, however, is the possibility that certain reviews might be manipulated. Developers of higher priced games 
-    might be under pressure to showcase a polished product, and for that reason might have bots add positive reviews of the product. 
-    This is one limitation of our analysis.
-    
-    - To go even further into our investigation, we decided to see if there exists any variation between genres for each of these 
-    three websites’ user ratings. For sake of eliminating edge cases, we decided to ignore any games that are more than $70, as 
-    there are very few. After plotting and examining our results, we found that there is little correlation between user ratings 
-    and price of each video game. It looked like the ratings were more variable when observing the results from the Steam user
-    ratings, and there was less correlation between price and user scores when looking at either Metacritic or IGDB. This is 
-    somewhat interesting due to the fact that Steam is the most popular of the three websites and this would hypothetically lead
-    to more watered-down results. This might be because of manipulation, since Steam is the actual platform where users buy games 
-    and it would be more advantageous for developers to boost ratings on Steam. Due to our lack of significant findings in our 
-    research so far, we hope to locate a dataset or more information, directly from the Steam API, that will help us discover 
-    more about how the size of developers influences user ratings, and also how the user ratings of cheap or expensive games 
-    change over time.
-    "
-  })
-  
   #----------------TAB1---------------------------------------------------------------------------------------------------------------------------
   
-  # Generate plot TAB 1 based on user selection
+  output$info_paragraph <- renderUI({
+    fluidRow(
+      column(12, 
+             tags$div(
+               style = "font-family: Arial, sans-serif; line-height: 1.5;",
+               
+               tags$h2("About Our Project"),
+               
+               tags$p("Our project is focused on one of the largest sources of creative output of the 21st century: video games. 
+                    Video games frequently employ detailed narratives, sophisticated and challenging gameplay, and can connect 
+                    with profound elements of the human psyche. We’ve decided to look at trends in video games as a way to
+                    assess what draws people to particular games."),
+               
+               tags$p("In large part, we’ve kept our exploration open-ended, but we’ve focused on the factors that influence the 
+                    ratings and popularity of video games on Steam, one of the largest platforms for purchasing and playing video 
+                    games, with an estimated 60% of total PC game revenue captured by the platform.* "),
+               
+               tags$hr(),
+               
+               tags$h2("Key Findings"),
+               
+               tags$h3("Price vs. Developer Size"),
+               tags$p("At first glance, we were interested in any possible relationship between price and developer size. However, 
+                    a clear relationship is not apparent. It is interesting to note that there are relatively few large developers 
+                    (measured by the number of Steam games published), which may warrant further investigation. It might be worthwhile 
+                    to look at large developers and small developers separately."),
+               
+               tags$h3("Price vs. User Ratings"),
+               tags$p("Diving in deeper to the data, we decided to investigate price vs user ratings across three different websites. 
+                    We chose to look closely at the ratings from users on Steam (of course), IGDB, and Metacritic. Each of these 
+                    websites are highly active and have a large number of users. Our rationale was that different categories of games 
+                    may inherently have higher development costs, or may  be subject to different kinds of pressures from their users. 
+                    Overall, we found a weak, positive relationship between price and popularity. One possibility for this relationship 
+                    existing at all, however, is the possibility that certain reviews might be manipulated. Developers of higher priced games 
+                    might be under pressure to showcase a polished product, and for that reason might have bots add positive reviews of the product. 
+                    This is one limitation of our analysis."),
+               
+               tags$h3("Genre Variations"),
+               tags$p("To go even further into our investigation, we decided to see if there exists any variation between genres for each of these 
+                    three websites’ user ratings. For sake of eliminating edge cases, we decided to ignore any games that are more than $70, as 
+                    there are very few. After plotting and examining our results, we found that there is little correlation between user ratings 
+                    and price of each video game. It looked like the ratings were more variable when observing the results from the Steam user
+                    ratings, and there was less correlation between price and user scores when looking at either Metacritic or IGDB. This is 
+                    somewhat interesting due to the fact that Steam is the most popular of the three websites and this would hypothetically lead
+                    to more watered-down results. This might be because of manipulation, since Steam is the actual platform where users buy games 
+                    and it would be more advantageous for developers to boost ratings on Steam. Due to our lack of significant findings in our 
+                    research so far, we hope to locate a dataset or more information, directly from the Steam API, that will help us discover 
+                    more about how the size of developers influences user ratings, and also how the user ratings of cheap or expensive games 
+                    change over time.")
+            )
+         )
+      )
+  })
+  
+  #----------------TAB2--------------------------------------------------------------------------------------------------------------------------
+  
+  output$directions_page <- renderUI({ 
+      fluidRow(
+        column(12, 
+               tags$div(
+                 style = "font-family: Arial, sans-serif; line-height: 1.5;",
+                 
+                 tags$h2("How to Use Our Tool"),
+                 
+                 tags$h3("Explore Steam Data page"),
+                 tags$p("Use this page to explore various aspects of the Steam API dataset. By using the dropdown selector tool on the 
+                 lefthand side of the page, you can choose to view either a density plot of prices of video games on Steam, a plot that
+                 displays game developer size vs price of games on Steam, or a plot that displays the average price of games on Steam over 
+                 time."),
+                 
+                 tags$h3("Explore Price to User Rating Page"),
+                 tags$p("Use this page to explore the pricing of video games on Steam vs their user rating. You can use the selector tools
+                 on the lefthand side of the page to change which user ratings (from 3 different websites) to view, which genre of game you 
+                 want to examine, and the pricing value that you want to limit the graph to.")
+                 
+               ))
+      )
+    })
+  
+  #----------------TAB3---------------------------------------------------------------------------------------------------------------------------
+  
+  # Generate plot TAB 2 based on user selection
   output$explore_plots <- renderPlot({
     plot <- NULL
     
@@ -181,9 +245,9 @@ server <- function(input, output) {
     plot
   })
   
-  #----------------TAB2---------------------------------------------------------------------------------------------------------------------------
+  #----------------TAB4---------------------------------------------------------------------------------------------------------------------------
   
-  # Generate plot TAB 2 based on user selection
+  # Generate plot TAB 3 based on user selection
   output$price_to_rating <- renderPlot({
     
     titleholder = ""
@@ -218,7 +282,7 @@ server <- function(input, output) {
     plot
   })
   
-  #----------------TAB3---------------------------------------------------------------------------------------------------------------------------
+  # END SERVER LOGIC ---------------------------------------------------------------------------------------------------------------------------
   
 }
 
