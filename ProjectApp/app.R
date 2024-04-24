ad96e53e5408f39bb6283931f95d7f03736d00c1
davinchi73 <dev107c.d133@gmail.com>
2024-04-24 18:09:37 -0500

    minor bug fixes for app

--- ProjectApp/app.R
+++ ProjectApp/app.R
@@ -25,7 +25,10 @@ steamDataClean <- read_csv("../../data/data_clean/steamDataClean.csv")
 steamDataGenres <- read_csv("../../data/data_clean/steamDataGenres.csv")
 steamDataDevelopers <- read_csv("../../data/data_clean/steamDataDevelopers.csv")
 
-# functions
+
+# functions -------------------------------------------------------------------------------------------------------------------------------
+
+# used in graphics
 genre_stats <- function(metric, source, genre) {
   data <- steamDataGenres %>%
     filter(genre == genre) %>%
@@ -46,6 +49,7 @@ genre_stats <- function(metric, source, genre) {
   return(stats)
 }
 
+# used in graphics
 source_stats <- function(source1, source2) {
   data <- steamDataGenres %>% 
     select(any_of(c(source1, source2))) %>% 
@@ -64,6 +68,10 @@ source_stats <- function(source1, source2) {
   
   return(stats)
 }
+
+
+# functions end -----------------------------------------------------------------------------------------------------------------------------
+
 
 # ui design start here
 ui <- fluidPage(
@@ -88,25 +96,14 @@ ui <- fluidPage(
       ),
       
       # TAB3 -----------------------------------------------------------------------------------------------------------------------------------
-      
-      
       tabPanel("Examining Price Distribution",
                
                uiOutput("density_page")
-               
-      ),
-      
-      # TAB4 -----------------------------------------------------------------------------------------------------------------------------------
-      
-      tabPanel("Limitations and Future Implementations",
-               
-               uiOutput("limitations_page")
                
       )
       
       # END TABS --------------------------------------------------------------------------------------------------------------------------------
     )
-    
 )
 
 # Define server logic required to draw a histogram
@@ -114,8 +111,12 @@ server <- function(input, output) {
   
   #----------------TAB1---------------------------------------------------------------------------------------------------------------------------
   
+  
+  # server logic to handle UI design of introduction page
   output$info_paragraph <- renderUI({
     fluidRow(
+      
+      #HTML style code for formatting the information page to introduce project to user
       column(12, 
              tags$div(
                style = "font-family: Arial, sans-serif; line-height: 1.5;",
@@ -172,12 +173,14 @@ server <- function(input, output) {
       )
   })
   
+  
+  # server logic for summarising plot of price to userscore
   output$big_price_plotO <- renderPlot({
     
+    # setup and vector creation
     genres <- c("Action", "Adventure", "Casual", "Racing", "Indie", "RPG", "Simulation")
     sources <- c("igdb_uscore", "meta_uscore", "store_uscore")
     inputs <- expand_grid(genres, sources)
-    
     pop_price_genre <- map2(inputs$sources, inputs$genres, genre_stats, metric = "full_price") %>% 
       bind_rows()
     
@@ -187,6 +190,7 @@ server <- function(input, output) {
       summarize(avg = mean(cor)) %>% 
       arrange(avg)
     
+    # create plot for pop price genre used to summarise price to user score
     plot <- pop_price_genre %>% 
       mutate(genre = factor(genre, levels = c("Racing", "Adventure", "Casual", "Indie", "RPG", "Action", "Simulation"))) %>% 
       ggplot() +
@@ -194,22 +198,31 @@ server <- function(input, output) {
       geom_errorbar(aes(x = genre, ymin = min, ymax = max, color = source), position = "dodge") +
       theme_classic()
     
+    # plot call
     plot
     
   })
   
   
+  # server logic for price density plot for info page
   output$price_dense_intro <- renderPlot({
+    
+    # create plot of price density for info page
     plotto <- steamDataClean %>% 
       ggplot() +
       geom_density(aes(x = full_price), fill = "#C5C3C0") +
       labs(x = "Price of Game (USD, excluding discounts)", y = "Proportion of Games") +
       theme_classic()
     
+    # plot
     plotto
   })
   
+  
+  # server logic for price to year plot
   output$priceToYear <- renderPlot({
+    
+    # data manipulation and setup
     avg_price <- steamDataClean %>%  
       mutate(published_store = ymd(published_store)) %>%
       mutate(year = year(published_store)) %>% 
@@ -217,6 +230,7 @@ server <- function(input, output) {
       mutate(avg_price = mean(full_price), cases = n()) %>% 
       filter(year > 2009)
     
+    # create price to year plot
     plottski <- ggplot(avg_price) +
       geom_bar(aes(x = year, y = avg_price/cases, fill = cases), stat = "identity") +
       #geom_text(aes(x = year, y = avg_price, label = cases), vjust = -.5) +
@@ -224,11 +238,15 @@ server <- function(input, output) {
       scale_fill_gradient(high = "#4C6B22", low = "#D2E885") +
       theme_classic()
     
+    # call to plot
     plottski
   })
   
+  
+  # server logic for correlation plot output
   output$correlations <- renderPlot({
     
+    # data manipulation for correlation plot
     correlations <- steamDataGenres %>% 
       select("igdb_score", "meta_score", "igdb_uscore", "meta_uscore", "store_uscore", "gfq_rating") %>%
       rename("IGDB" = igdb_score, 
@@ -240,12 +258,18 @@ server <- function(input, output) {
       drop_na() %>% 
       cor()
     
+    # create correlation matrix plot
     plotaroo <- corrplot(correlations, method = "number", order = "hclust", tl.col = "black", tl.srt = 45)
     
+    # call to plot
     plotaroo
   })
   
+
+  # server logic for creating score averages plot
   output$score_ave <- renderPlot({
+    
+    # data manipulation and setup for plot
     score_avg <- steamDataGenres %>%
       pivot_longer(cols = c(meta_score, meta_uscore, igdb_score, igdb_uscore), names_to = "score_name") %>% 
       group_by(score_name) %>% 
@@ -253,6 +277,7 @@ server <- function(input, output) {
       mutate(score_type = ifelse(str_detect(score_name, "uscore"), "Critic Rating", "User Rating")) %>% 
       mutate(source = ifelse(str_detect(score_name, "igdb"), "IGDB", "Metacritic"))
     
+    # create plot for score averages
     plotskittles <- ggplot(score_avg) +
       geom_bar(aes(x = score_name, y = avg, fill = score_type), stat = "identity") +
       facet_wrap(~source, scales = "free_x") +
@@ -260,13 +285,17 @@ server <- function(input, output) {
       theme_classic() +
       scale_fill_manual(values = c("#325974", "#67C1F5"))
     
+    # call to plot
     plotskittles
   })
   
   #----------------TAB2--------------------------------------------------------------------------------------------------------------------------
   
+  # UI output server logic for interactive price vs userscore page
   output$plot_page <- renderUI({
     fluidRow(
+      
+      #selection and slider inputs
       column(6,
              selectInput("rating_select", "Select rating", 
                          choices = c("Steam Userscore", 
@@ -287,6 +316,8 @@ server <- function(input, output) {
                          max = max(steamDataGenres$full_price),
                          value = 50)
       ),
+      
+      #HTML style objects for user directions
       column(6,
              tags$div(
                style = "font-family: Arial, sans-serif; line-height: 1.5;",
@@ -297,6 +328,8 @@ server <- function(input, output) {
              want to examine, and the pricing value that you want to limit the graph to.")
              ),
       ),
+      
+      # calls to plot outputs for price to userscore page
       column(12,
              plotOutput("price_to_rating"),
              plotOutput("big_price_plot")
@@ -304,9 +337,11 @@ server <- function(input, output) {
     )
   })
   
-  # Generate plot based on user selection
+  
+  # plot output server logic to handle interactive price vs userscore plot
   output$price_to_rating <- renderPlot({
     
+    # handles userscore user input choice selection bar
     titleholder = ""
     if (input$rating_select == "Steam Userscore") {
       titleholder = "store_uscore"
@@ -316,14 +351,16 @@ server <- function(input, output) {
       titleholder = "meta_uscore"
     }
     
+    # declare plot
     plot <- NULL
     
+    # if statement to handle in case user selects All genres
     if (input$genre_select == "All") {
       
       cleanData <- steamDataGenres %>% 
         select(full_price, !!sym(titleholder)) %>%
         filter(full_price <= input$price_select, !is.na(!!sym(titleholder) <= 250))
-      
+
     } else {
       
       cleanData <- steamDataGenres %>% 
@@ -331,6 +368,7 @@ server <- function(input, output) {
         filter(!is.na(!!sym(input$genre_select)), full_price <= input$price_select, !!sym(titleholder) <= 250)
     }
     
+    # create plot based on user input for price vs userscore of choice
     plot <- ggplot(cleanData) + 
       geom_point(aes(x = full_price, y = !!sym(titleholder))) +
       stat_smooth(aes(x = full_price, y = !!sym(titleholder)), method = "lm", se = TRUE) +
@@ -346,15 +384,16 @@ server <- function(input, output) {
     
     # Return the plot
     plot
-    
   })
   
+  
+  # informative graph summarizing price vs userscore
   output$big_price_plot <- renderPlot({
     
+    # setup for viz
     genres <- c("Action", "Adventure", "Casual", "Racing", "Indie", "RPG", "Simulation")
     sources <- c("igdb_uscore", "meta_uscore", "store_uscore")
     inputs <- expand_grid(genres, sources)
-    
     pop_price_genre <- map2(inputs$sources, inputs$genres, genre_stats, metric = "full_price") %>% 
       bind_rows()
     
@@ -364,6 +403,7 @@ server <- function(input, output) {
       summarize(avg = mean(cor)) %>% 
       arrange(avg)
     
+    # plot
     plot <- pop_price_genre %>% 
       mutate(genre = factor(genre, levels = c("Racing", "Adventure", "Casual", "Indie", "RPG", "Action", "Simulation"))) %>% 
       ggplot() +
@@ -372,21 +412,25 @@ server <- function(input, output) {
       theme_classic()
     
     plot
-    
   })
+  
   
   #----------------TAB3---------------------------------------------------------------------------------------------------------------------------
   
   
+  # price density user interactive graph UI server definition
   output$density_page <- renderUI({
     fluidRow(
       column(6,
+             
+             #inputs for user design
              selectInput("rating_selectD", "Select rating", 
                          choices = c("Steam Userscore", 
                                      "IGDB Userscore", 
                                      "Metacritic Userscore")),
              selectInput("genre_selectD", "Select game genre", 
-                         choices = c("Adventure", 
+                         choices = c("All",
+                                     "Adventure", 
                                      "Casual", 
                                      "Action",
                                      "Racing",
@@ -396,7 +440,7 @@ server <- function(input, output) {
              sliderInput("price_selectD",
                          "Price parameter",
                          min = 0,
-                         max = 70,
+                         max = max(steamDataGenres$full_price),
                          value = 35),
              sliderInput("pop_max", 
                          "Max Popularity",
@@ -409,6 +453,8 @@ server <- function(input, output) {
                          max = 99,
                          value = 0)
              ),
+      
+      #HTML style logic for user directions
       column(6,
              tags$div(
                style = "font-family: Arial, sans-serif; line-height: 1.5;",
@@ -420,14 +466,19 @@ server <- function(input, output) {
                you would like to examine.")
              ),
       ),
+      
+      # call to price density plot
       column(12,
              plotOutput("price_density")
             )
       )
   })
   
+  
+  # price density user interactive graph server definition
   output$price_density <- renderPlot({
     
+    # handles userscore user input setting
     titleholderD = ""
     if (input$rating_selectD == "Steam Userscore") {
       titleholderD = "store_uscore"
@@ -437,13 +488,24 @@ server <- function(input, output) {
       titleholderD = "meta_uscore"
     }
     
-    cleanData <- steamDataGenres %>% 
-      select(full_price, !!sym(input$genre_selectD), !!sym(titleholderD)) %>%
-      filter(!is.na(!!sym(input$genre_selectD)), 
-             full_price <= 70,
-             !!sym(titleholderD) <= input$pop_max,
-             !!sym(titleholderD) >= input$pop_min)
+    #handles if genre select is set to all
+    if (input$genre_selectD == "All") {
+      cleanData <- steamDataGenres %>% 
+        select(full_price, !!sym(titleholderD)) %>%
+        filter(full_price <= input$price_selectD,
+               !!sym(titleholderD) <= input$pop_max,
+               !!sym(titleholderD) >= input$pop_min)
+    } else {
+      # handles genre setting and min/max setting
+      cleanData <- steamDataGenres %>% 
+        select(full_price, !!sym(input$genre_selectD), !!sym(titleholderD)) %>%
+        filter(!is.na(!!sym(input$genre_selectD)), 
+               full_price <= input$price_selectD,
+               !!sym(titleholderD) <= input$pop_max,
+               !!sym(titleholderD) >= input$pop_min)
+    }
     
+    # plot is plotted using the set settings
     plotski <- ggplot(cleanData) + 
       geom_density(aes(x = full_price)) +
       labs(title = str_c("Price density of ", as.character(input$genre_selectD), " Steam Games, in terms of ", as.character(input$rating_selectD)), x = "Full Price of Video Games", y = "Density") +
@@ -454,29 +516,10 @@ server <- function(input, output) {
         axis.title = element_text(face = "bold"),
         legend.position = "bottom")
     
+    # call to plot
     plotski
   })
   
-  #----------------TAB4---------------------------------------------------------------------------------------------------------------------------
-  
-  
-  output$limitations_page <- renderUI({
-    
-    fluidRow(
-      column(12, 
-             tags$div(
-               style = "font-family: Arial, sans-serif; line-height: 1.5;",
-               
-               tags$h3("Limitations to Our Research"),
-               tags$p("TBD"),
-               
-               tags$h3("Possible Future Implementations"),
-               tags$p("TBD")
-               
-             ))
-    )
-    
-  })
   
   # END SERVER LOGIC ---------------------------------------------------------------------------------------------------------------------------
   
